4888561de9fe697d502760bdf719021f2a7f2b30
jeison <jeisonfelipecas08gmail.com>
2025-04-09 11:53:26 -0500

    arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Login from './components/Login';
-
-import Home from './components/Home/Home.jsx';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Pedidos/Home.jsx';
-
 import Admin from './components/admin/Admin';
 import Inventario from './components/admin/inventario.jsx';
 import AdCrear from './components/admin/adcrear.jsx'; 
-import Tecnico from './components/Tecnico/Tecnico';
-
+import Tecnico from './components/Tecnico/Tecnico.jsx';
 import Pie from './components/Tecnico/Pie.jsx';
 import Tercera from './components/Tecnico/tercera.jsx';
 import Cuarta from '/src/components/Tecnico/Cuarta.jsx'; 
 import Quinta from './components/Tecnico/Quinta.jsx';
-
-
-
-import Soliespacios from './components/Home/Espacios/Solicitud_espacios';
-import Soli_port from './components/Home/Soli_Port.jsx';
 import Solitelevisores from './components/Home/Pedidos/Solici_televisor.jsx';
 import Soliespacios from './components/Home/Espacios/Solicitud_espacios';
-
 import Soli_port from './components/Home/Soli_Port.jsx';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
@@ -35,47 +21,27 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
-        <Route path='/login' element={<Login />} />
-        <Route path='/Usuario' element={<Home />} />
-        <Route path='/Admin' element={<Admin />} />
-        <Route path='/Tecnico' element={<Tecnico />} />
         <Route path='/Pie' element={<Pie />} /> 
         <Route path='/Tercera' element={<Tercera />} />
         <Route path='/Cuarta' element={<Cuarta />} />
         <Route path='/Quinta'element={<Quinta />} />
-      </Routes>
-
-        <Route path='/' element={<Login />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />  
         <Route path='/Usuario' element={<Home />} />
-        <Route path='/Admin' element={<Admin />} />
         <Route path='/inventario' element={<Inventario />} />
         <Route path='/adcrear' element={<AdCrear />} />
-        <Route path='/Tecnico' element={<Tecnico />} />
-        <Route path='/Solicitar-Portatiles' element={<Soli_port />} />
-      </Routes>
-
         <Route path='/login' element={<Login />} />
-        <Route path='/Usuario' element={<Home />} />
         <Route path='/Admin' element={<Admin />} />
         <Route path='/Tecnico' element={<Tecnico />} />
         <Route path='/espacios' element={<Soliespacios />} />
         <Route path='/Solicitar-Portatiles' element={<Soli_port />} />
         <Route path='/Solicitartelevisores' element={<Solitelevisores/>} />
-      </Routes>
-    <Routes>
-      <Route path='/login' element={<Login></Login>}></Route>
-      <Route path='/Usuario' element={<Home></Home>}></Route>
-      <Route path='/Admin' element={<Admin></Admin>}></Route>
-      <Route path='/Tecnico' element={<Tecnico></Tecnico>}></Route>
-
-      <Route path="/espacios" element={<Soliespacios />}></Route>
-
-      <Route path='/Solicitar-Portatiles' element={<Soli_port></Soli_port>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/Usuario' element={<Home></Home>}></Route>
+        <Route path='/Admin' element={<Admin></Admin>}></Route>
+        <Route path='/Tecnico' element={<Tecnico></Tecnico>}></Route>
+        <Route path="/espacios" element={<Soliespacios />}></Route>
+        <Route path='/Solicitar-Portatiles' element={<Soli_port></Soli_port>}></Route>
     </Routes>
-
-
     </BrowserRouter>
   );
 }
