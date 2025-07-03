9c640598984b5101a4eaf9417a1721d43e081809
adriXO06 <adriandjpc@hotmail.com>
2025-07-03 00:46:39 -0500

    Reapply "xd"
    This reverts commit ac9adb90aa90f9ee9f066340ee5cf5814e08de07.

--- src/components/admin/header_inv/header_inv.jsx
+++ src/components/admin/header_inv/header_inv.jsx
@@ -1,56 +1,60 @@
-import React, { useState } from 'react';
-import { Navbar, Container, Nav, Button, Offcanvas } from 'react-bootstrap';
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './header_inv.css'; // This CSS file will now contain the new class styles
+import './header_inv.css';
 import Desplegable from '../../desplegable/desplegable';
-import { Link } from 'react-router-dom';
 
-function Header_inv() {
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+function header_inv() {
   return (
-    <div className='header1001'> {/* Changed to header1001 */}
-      <Navbar expand="xxxl"> {/* Removed header100002 as it's not in the new structure */}
+    <div className='header100001'> 
+
+      <Navbar expand="xxxl" className='header100002'>
         <Container>
-          <Button variant="primary" className='Icon_menu' onClick={handleShow}> {/* Changed to Icon_menu */}
-            <i className="bi bi-list"></i>
-          </Button>
-          <Offcanvas show={show} onHide={handleClose}>
-            <Offcanvas.Header className='header_menuuu'>
-              <Offcanvas.Title className='header_menu'><h1>Menú</h1></Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              {/* Changed to cuadrito1 */}
-              <Link to="/Admin" className='cuadrito1'>
-                <h3><i className="bi bi-ticket-detailed"></i> Estado del ticket</h3>
-              </Link>
-              <Link to="/Adcrear" className='cuadrito1'>
-                <h3><i className="bi bi-person-plus"></i> Gestionar usuarios</h3>
-              </Link>
-              <Link to="/Inventario" className='cuadrito1'>
-                <h3><i className="bi bi-box-seam"></i> Consultar inventario</h3>
-              </Link>
-            </Offcanvas.Body>
-          </Offcanvas>
+          <Navbar.Toggle className='header100003' />
+          <Navbar.Collapse className='header100004'>
+            <Nav>
+
+            <div>
+              
+              <div className='header100005'>
+              <NavDropdown.Item href="#action/3.1">Menu</NavDropdown.Item>
+              </div>
+              <div  className='header100006'>
+              <NavDropdown.Item href='/Admin' >
+                Estado del ticket
+              </NavDropdown.Item >
+              </div>
+              <div  className='header100006'>
+              <NavDropdown.Item href='/Adcrear' >
+                Gestionar usuarios
+              </NavDropdown.Item >
+              </div>
+              <div className='header100006'>
+              <NavDropdown.Item href="/Inventario">
+                Consultar inventario
+              </NavDropdown.Item>
+              </div>
+              <NavDropdown.Divider />
+              </div>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
+
       <Navbar>
         <Container>
-          {/* Changed to header1007 but keeping "Inventario" text */}
-          <h1 className='header1007'>Inventario</h1>
-          <Container className='Iconos'>
-            <Nav.Link href="http://localhost:5173/Login">Home</Nav.Link> {/* Removed header100008 */}
-            <Nav.Link className="Blogc" href="https://electricidadelectronicaytelecomu.blogspot.com/">Blog CEET</Nav.Link> {/* Changed to Blogc */}
-            <Desplegable />
-          </Container>
+          <h1 className='header100007'>Inventario</h1>
+          
+            <Nav.Link href="http://localhost:5173/Login" className='header100008'>Home</Nav.Link>
+            <Nav.Link href="https://electricidadelectronicaytelecomu.blogspot.com/" className='header100009'>Blog CEET</Nav.Link>
+        <Desplegable></Desplegable>
+        
         </Container>
       </Navbar>
     </div>
   );
 }
 
-export default Header_inv;
+export default header_inv;
