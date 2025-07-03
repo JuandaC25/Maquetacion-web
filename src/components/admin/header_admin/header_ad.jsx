be40675ba76452f0c2a86434448182debb44a15f
adriXO06 <adriandjpc@hotmail.com>
2025-07-03 00:24:22 -0500

    header modificado
    los tres headers estan tal cual como lo necesitan

--- src/components/admin/header_admin/header_ad.jsx
+++ src/components/admin/header_admin/header_ad.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'; // Added useState import
-import { Navbar, Container, Nav, Button, Offcanvas } from 'react-bootstrap'; // Added Button and Offcanvas imports
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, Button, Offcanvas } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './header_ad.css';
+import './header_ad.css'; // This CSS file will now be identical to header_inv.css
 import Desplegable from '../../desplegable/desplegable';
-import { Link } from 'react-router-dom'; // Added Link import
+import { Link } from 'react-router-dom';
 
 function Header_crear() {
   const [show, setShow] = useState(false);
@@ -12,24 +12,24 @@ function Header_crear() {
   const handleShow = () => setShow(true);
 
   return (
-    <div className='header601'> 
-      <Navbar expand="xxxl" className='header602'>
+    <div className='header1001'> {/* Class changed to header1001 to match Header_inv */}
+      <Navbar expand="xxxl"> {/* Removed explicit Navbar className to match Header_inv's structure */}
         <Container>
-          <Button variant="primary" className='header603' onClick={handleShow}>
+          <Button variant="primary" className='Icon_menu' onClick={handleShow}> {/* Class changed to Icon_menu */}
             <i className="bi bi-list"></i>
           </Button>
           <Offcanvas show={show} onHide={handleClose}>
-            <Offcanvas.Header className='header604'> {/* Changed from header_menuuu */}
-              <Offcanvas.Title className='header605'><h1>Menú</h1></Offcanvas.Title> {/* Changed from header_menu */}
+            <Offcanvas.Header className='header_menuuu'> {/* Class changed to header_menuuu */}
+              <Offcanvas.Title className='header_menu'><h1>Menú</h1></Offcanvas.Title> {/* Class changed to header_menu */}
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Link to="/Admin" className='header606'> {/* Changed from header90006 */}
+              <Link to="/Admin" className='cuadrito1'> {/* Class changed to cuadrito1 */}
                 <h3><i className="bi bi-ticket-detailed"></i> Estado del ticket</h3>
               </Link>
-              <Link to="/Adcrear" className='header606'> {/* Changed from header90006 */}
+              <Link to="/Adcrear" className='cuadrito1'> {/* Class changed to cuadrito1 */}
                 <h3><i className="bi bi-person-plus"></i> Gestionar usuarios</h3>
               </Link>
-              <Link to="/Inventario" className='header606'> {/* Changed from header90006 */}
+              <Link to="/Inventario" className='cuadrito1'> {/* Class changed to cuadrito1 */}
                 <h3><i className="bi bi-box-seam"></i> Consultar inventario</h3>
               </Link>
             </Offcanvas.Body>
@@ -39,10 +39,13 @@ function Header_crear() {
 
       <Navbar>
         <Container>
-          <h1 className='header607'>Estado De Ticket</h1> {/* Changed from header90007 */}
-          <Nav.Link href="http://localhost:5173/Login" className='header608'>Home</Nav.Link> {/* Changed from header90008 */}
-          <Nav.Link href="https://electricidadelectronicaytelecomu.blogspot.com/" className='header609'>Blog CEET</Nav.Link> {/* Changed from header90009 */}
-          <Desplegable></Desplegable>
+          {/* Class changed to header1007, but text remains "Estado De Ticket" */}
+          <h1 className='header1007'>Estado De Ticket</h1>
+          <Container className='Iconos'> {/* Added Iconos container */}
+            <Nav.Link href="http://localhost:5173/Login">Home</Nav.Link> {/* No class for Home in Header_inv's final structure */}
+            <Nav.Link className="Blogc" href="https://electricidadelectronicaytelecomu.blogspot.com/">Blog CEET</Nav.Link> {/* Class changed to Blogc */}
+            <Desplegable />
+          </Container>
         </Container>
       </Navbar>
     </div>
