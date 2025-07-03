26a6dab5016b11597f12a3d8ccc913907d57857d
anderson <adriandjpc@hotmail.com>
2025-07-03 11:42:13 -0500

    esta listo los estils de admin
    configure todos los estilos de admin para ya estar terminado

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+                <Route path='/Login' element={<Login/>}/> 
         <Route path='/' element={<Login/>}/> 
         <Route path='/Tercera' element={<Tercera />} />
         <Route path='/Cuarta' element={<Cuarta />} />
