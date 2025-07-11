aa4d315c9695caf7a070b3aa0a1cc6cffee8cf91
Alejandro <johanalejandrobernal@gmail.com>
2025-07-11 17:07:11 -0500

    Añadi animaciones e imagenes

--- src/components/Home/Pedidos/Cua_pedidos/Cuadro_pedidos.jsx
+++ src/components/Home/Pedidos/Cua_pedidos/Cuadro_pedidos.jsx
@@ -6,35 +6,51 @@ function Cuadro_Pedidos() {
   return (
     <div className='Cuadros'> 
       <div className='fila-superiorr'>
-        <Card className='cuadro1'>
+
+        
+        <Card className="CuadroP0rt">
+          <div className="background-effect-port"></div>
+          <img src="/imagenes/portatil.png" className="imagen-animada-port" alt="Portátil" />
           <Card.Body>
-            <Card.Title><h1 className='portatiles'>Portátiles</h1></Card.Title>
-            <Button href='/Solicitar-Portatiles' className='boton1'>Seleccionar</Button>
+            <Card.Title className='Tit-001'><h4>Portátiles</h4></Card.Title>
+            <Button href='/Solicitar-Portatiles' className='bot00n1'>Seleccionar</Button>
           </Card.Body>
         </Card>
-        <Card className='cuadro2'>
+  
+        <Card className="CuadroEscr">
+          <div className="background-effect-escr"></div>
+          <img src="/imagenes/Equipo de escritorio.png" className="imagen-animada-escr" alt="Escritorio" />
           <Card.Body>
-            <Card.Title><h2 className='escritorio'>Equipos de escritorio</h2></Card.Title>
-            <Button href='/Pedidoescritorio'className='boton_esc'>Seleccionar</Button>
+            <Card.Title className='Tit-002'><h4>Equipos de escritorio</h4></Card.Title>
+            <Button href='/Pedidoescritorio' className='boton_esc'>Seleccionar</Button>
           </Card.Body>
         </Card>
       </div>
-      <div className='fila-inferiorr'>
-        <Card className='cuadro3'>
+
+      <div className='fila-inferiorr'>      
+        <Card className="CuadroTV">
+          <div className="background-effect-tv"></div>
+          <img src="/imagenes/Televisor.png" className="imagen-animada-tv" alt="Televisor" />
           <Card.Body>
-            <Card.Title><h1 className='televisores'>Televisores</h1></Card.Title>
+            <Card.Title className='Tit-003'><h4>Televisores</h4></Card.Title>
             <Button href='/Solicitartelevisores' className='boton_TV'>Seleccionar</Button>
           </Card.Body>
         </Card>
-              <Card className='cuadro4'>
+
+        
+        <Card className="CuadroElem">
+          <div className="background-effect-elem"></div>
+          <img src="/imagenes/Accesorios.png" className="imagen-animada-elem" alt="Elementos" />
           <Card.Body>
-            <Card.Title><h1 className='Elementos'>Elementos</h1></Card.Title>
+            <Card.Title className='Tit-004'><h4>Elementos</h4></Card.Title>
             <Button href='/Pedidos_ele' className='boton_elements'>Seleccionar</Button>
           </Card.Body>
         </Card>
+
       </div>
     </div>
   );
 }
 
 export default Cuadro_Pedidos;
+
