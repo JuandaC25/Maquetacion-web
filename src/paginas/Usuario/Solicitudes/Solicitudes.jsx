a97b977367cd95437d6bd1f3e27384102fa57c2f
Yoan <yoarobert1@gmail.com>
2025-09-29 16:58:00 -0500

    Se añadieron archivos

---
+++ src/paginas/Usuario/Solicitudes/Solicitudes.jsx
@@ -0,0 +1,10 @@
+import Cuadro_Pedidos from "../../../components/Home/Pedidos/Cua_pedidos/Cuadro_pedidos"
+
+export default function SolicitudesGeneral () {
+    return(
+        <>
+        
+        <Cuadro_Pedidos/>
+        </>
+    )
+}
