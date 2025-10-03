6d47aac9af4ba5ca0627dd86814fbb909829c6ce
Alejandro <johanalejandrobernal@gmail.com>
2025-10-03 14:04:09 -0500

    Cambios con la terminacion de varios apartados

--- src/components/Home/Espacios/Datos_pedido.jsx
+++ src/components/Home/Espacios/Datos_pedido.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import "./Solicitud_espacios.css";
 import Card from 'react-bootstrap/Card'; 
-import { Carousel, Modal, Button } from 'react-bootstrap';
+import { Carousel, Modal, Button, CarouselItem } from 'react-bootstrap';
+import espacio1 from "/imagenes/imagenes_espacios/espacio1.jpeg";
+import espacio2 from '/imagenes/imagenes_espacios/espacio2.jpeg';
+import espacio3 from '/imagenes/imagenes_espacios/espacio3.jpeg';
+import espacio4 from '/imagenes/imagenes_espacios/espacio4.jpeg';
+import espacio5 from '/imagenes/imagenes_espacios/espacio5.jpeg';
+import Auditorio1 from '/imagenes/imagenes_espacios/Auditorio1.jpeg';
+import Auditorio2 from '/imagenes/imagenes_espacios/Auditorio2.jpeg';
+import Auditorio3 from '/imagenes/imagenes_espacios/Auditorio3.jpeg';
+import Auditorio4 from '/imagenes/imagenes_espacios/Auditorio4.jpeg';
+
 
 function Datos_pedido() {
   const [showModal, setShowModal] = useState(false);
@@ -14,14 +24,35 @@ function Datos_pedido() {
       <div className="ACMC-Cua">
         <Card className="Cuadroparaespacio">
           <Carousel className='carrusel-espacios'>
-            <Carousel.Item>
+             <Carousel.Item>
               <img
-                src="/imagenes/Polideportivo.jpg"
-                alt="Polideportivo"
-                className="ImagenPolideportivo"
-              />
-            </Carousel.Item>
-          </Carousel>
+              src="/imagenes/Polideportivo.jpg"
+              alt="Polideportivo"
+              className="ImagenPolideportivo"
+            />
+          </Carousel.Item>
+              
+          <Carousel.Item>
+            <img src="/imagenes/imagenes_espacios/espacio1.jpeg" alt="Espacio 1" className='ImagenPolideportivo'/>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src="/imagenes/imagenes_espacios/espacio2.jpeg" alt="Espacio 2" className='ImagenPolideportivo' />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src="/imagenes/imagenes_espacios/espacio3.jpeg" alt="Espacio 3" className='ImagenPolideportivo'/>
+          </Carousel.Item>
+          
+          <Carousel.Item>
+            <img src="/imagenes/imagenes_espacios/espacio4.jpeg" alt="Espacio 4" className='ImagenPolideportivo'/>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img src="/imagenes/imagenes_espacios/espacio5.jpeg" alt="Espacio 5" className='ImagenPolideportivo'/>
+          </Carousel.Item>
+        </Carousel>
+        
           <div className="product-details">
             <h1 className="solicitud-titulo001">Solicitar Polideportivo</h1>
             <p> Instalacion deportiva que cuenta con varias infraestructuras y areas dedicadas a la practica de diferentes deportes y actividades fisicas. <br/><br/> <strong>Especificaciones del lugar</strong> <br/><br/> -Cuenta con una mallla de voleibol. <br/> -Posee dos arcos para practicar deportes como futbol <br/>o baloncesto. <br/> -Pantallas y equipos de sonido. </p>
@@ -60,13 +91,19 @@ function Datos_pedido() {
         <Card className="Cuadroparaauditorio">
           <Carousel className='carrusel-Auditorio'>
             <Carousel.Item>
-              <img src="/imagenes/Auditorio.jpeg" alt="Auditorio1" className="Imagenes-auditorio1" />
-            </Carousel.Item>
-            <Carousel.Item>
               <img src="/imagenes/Imagen1Auditorio.jpg" alt="Auditorio2" className="Imagenes-auditorio2" />
             </Carousel.Item>
             <Carousel.Item>
-              <img src="/imagenes/Imagen2Auditorio.jpg" alt="Auditorio3" className="Imagenes-auditorio3" />
+              <img src="/imagenes/imagenes_espacios/Auditorio1.jpeg" alt="Auditorio" className='Imagenes-auditorio1'/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="/imagenes/imagenes_espacios/Auditorio2.jpeg" alt="Auditorio" className='Imagenes-auditorio1'/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="/imagenes/imagenes_espacios/Auditorio3.jpeg" alt="Auditorio" className='Imagenes-auditorio1'/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="/imagenes/imagenes_espacios/Auditorio4.jpeg" alt="Auditorio" className='Imagenes-auditorio1'/>
             </Carousel.Item>
           </Carousel>
           <div className="product-details">
