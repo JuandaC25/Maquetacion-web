98ea8bb77ae7de6cc9d6fbfcd0dda2d946b96375
jeison <jeisonfelipecas08@gmail.com>
2025-10-06 02:50:07 -0500

    casi terminado

--- src/components/Tecnico/solicitudes_de_equipos/Modal_Confriamcion/ConfirmacionModal.jsx
+++ src/components/Tecnico/solicitudes_de_equipos/Modal_Confriamcion/ConfirmacionModal.jsx
@@ -12,12 +12,18 @@ function ConfirmacionModal({ show, onHide, onConfirm, mensaje }) {
           <h2>Confirmación</h2>
           <button className="confirmacion-close-btn" onClick={onHide}>×</button>
         </div>
+
         <div className="confirmacion-cuerpo">
           <p>{mensaje || '¿Deseas realizar esta acción?'}</p>
         </div>
+
         <div className="confirmacion-footer">
-          <button className="btn-verde-no" onClick={onHide}>No</button>
-          <button className="btn-verde-si" onClick={onConfirm}>Sí</button>
+          <button className="btn-verde-no" onClick={onHide}>
+            No
+          </button>
+          <button className="btn-verde-si" onClick={onConfirm}>
+            Sí
+          </button>
         </div>
       </div>
     </>
